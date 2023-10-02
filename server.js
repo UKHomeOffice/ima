46e9d0729763c044b35e2f5f781db9c287b194d0
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-10-02 14:23:01 +0100

    IMB-27:
    - amend drone.yml to match drone secrets in settings
    - add kube files
    - add initial unit test

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const config = require('./config.js');
 const busboy = require('busboy');
 const bytes = require('bytes');
 const bl = require('bl');
+const _ = require('lodash');
 
 
 let settings = require('./hof.settings');
@@ -19,9 +20,28 @@ const app = hof(settings);
 
 app.use((req, res, next) => {
   res.locals.htmlLang = 'en';
-  res.locals.feedbackUrl = '/feedback';
+  res.locals.feedbackUrl = '/https://eforms.homeoffice.gov.uk/outreach/feedback.ofml';
   next();
 });
+
+if (config.env === 'development' || config.env === 'test') {
+  app.use('/test/bootstrap-session', (req, res) => {
+    const appName = req.body.appName;
+
+    if (!_.get(req, 'session[`hof-wizard-${appName}`]')) {
+      if (!req.session) {
+        throw new Error('Redis is not running!');
+      }
+      req.session[`hof-wizard-${appName}`] = {};
+    }
+
+    Object.keys(req.body.sessionProperties || {}).forEach(key => {
+      req.session[`hof-wizard-${appName}`][key] = req.body.sessionProperties[key];
+    });
+
+    res.send('Session populate complete');
+  });
+}
 
 app.use((req, res, next) => {
   if (req.is('multipart/form-data')) {
