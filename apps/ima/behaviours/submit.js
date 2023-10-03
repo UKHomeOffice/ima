05d288a05af6c26a266c6ec98276fb767090fb6a
Mylrajan Muthusamy <mylrajan.muthusamy1@digital.homeoffice.gov.uk>
2023-10-03 09:06:32 +0100

    IMB-23 Download files from filevault using keycloak access
    - Add create-and-send-pdf behaviour
    - Add submit behaviour
    - Add confirmation view
    - Added pdf view
    - Add case worker email and submission templateid to config.js
    - Amend readme

---
+++ apps/ima/behaviours/submit.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const _ = require('lodash');
+const CreateAndSendPDF = require('./create-and-send-pdf');
+
+module.exports = superclass => class extends superclass {
+  successHandler(req, res, next) {
+    const uploadPdfShared = new CreateAndSendPDF({
+      sendReceipt: true,
+      sortSections: true
+    });
+    // don't await async process, allow user to move on
+    uploadPdfShared.send(req, res, super.locals(req, res));
+
+    const userEmail = req.sessionModel.get('user-email');
+    const userFormEmail = req.sessionModel.get('email-address-details');
+    const advisorEmail = req.sessionModel.get('legal-representative-email');
+    const allUniqueEmails = _.uniq([userEmail, userFormEmail, advisorEmail].filter(e => e));
+
+    req.sessionModel.set('all-unique-emails', allUniqueEmails);
+
+    return super.successHandler(req, res, next);
+  }
+};
--- config.js
+++ config.js
@@ -50,7 +50,9 @@ module.exports = {
   },
   govukNotify: {
     notifyApiKey: process.env.NOTIFY_STUB === 'true' ? 'USE_MOCK' : process.env.NOTIFY_KEY,
-    userAuthTemplateId: process.env.USER_AUTHORISATION_TEMPLATE_ID
+    userAuthTemplateId: process.env.USER_AUTHORISATION_TEMPLATE_ID,
+    caseworkerEmail: process.env.CASEWORKER_EMAIL,
+    submissionTemplateId: process.env.SUBMISSION_TEMPLATE_ID
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
