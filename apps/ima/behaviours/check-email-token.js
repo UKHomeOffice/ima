98b0b73852160c8235a6ea207c25d5a59c58e229
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-10-03 17:14:19 +0100

    IMB-25: UAN/DOB verification:
    - Add uan upload functionality
    
    IMB-28: Add frontend for UAN/DOB verification:
    - Amend ima/index.js and verify/index.js with relevant steps, fields and behaviours
    - Add validation to prevent different emails accessing an existing case at the same time
    - Add relevant views, behaviours, fields and pages config for cases, current progress and who-are-you steps to ima folder
    - Add relevant views, behaviours, validation, fields and pages config for your-details and verify and check-inbox steps to verify folder
    - Amend config.js to set up behaviours
    - add files for redis, getting and saving tokens to db folder
    - Amend summary-data-section
    - Add relevant tests

---
+++ apps/ima/behaviours/check-email-token.js
@@ -0,0 +1,43 @@
+'use strict';
+const getToken = require('../../../db/get-token');
+const config = require('../../../config');
+const _ = require('lodash');
+
+/* eslint no-process-env: 0*/
+module.exports = superclass => class extends superclass {
+  saveValues(req, res, next) {
+    const token = req.query.token;
+    const skipEmail = config.login.skipEmail;
+    const skipEmailAuth = token === 'skip' && config.login.allowSkip && skipEmail;
+    const validEmailToken = req.sessionModel.get('valid-token') === true;
+    const id = _.get(req.session['hof-wizard-verify'], 'uan');
+    // skips if it goes to /ima/start?token=skip
+    // skips if a session is already present.
+    if (skipEmailAuth && id) {
+      req.sessionModel.set('valid-token', true);
+      req.sessionModel.set('user-email', skipEmail);
+      req.sessionModel.set('uan', id);
+      req.sessionModel.set('date-of-birth', _.get(req.session['hof-wizard-verify'], 'date-of-birth'));
+      return super.saveValues(req, res, next);
+    }
+
+    if (validEmailToken) {
+      return super.saveValues(req, res, next);
+    }
+    // returns a Promise
+    return getToken.read(token)
+      .then(user => {
+        if (user.valid) {
+          // this is so a user can go back without requesting a new token
+          req.sessionModel.set('valid-token', true);
+          // store email to send to caseworker later
+          req.sessionModel.set('user-email', user.email);
+          req.sessionModel.set('uan', user.uan);
+          req.sessionModel.set('date-of-birth', user['date-of-birth']);
+          return super.saveValues(req, res, next);
+        }
+        return res.redirect(config.login.invalidTokenPath);
+      })
+      .catch(err => req.log('info', `Check Token Error: ${err}`));
+  }
+};
---
+++ test/_unit/verify/behaviours/send-verification-email.spec.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const config = require('../../../../config');
+const reqres = require('reqres');
+const proxyquire = require('proxyquire').noCallThru();
+const Controller = require('hof').controller;
+const NotifyClient = require('notifications-node-client').NotifyClient;
+
+describe('apps/verify/behaviours/send-verification-email', () => {
+  let axiosGetStub;
+  let req;
+  let res;
+  let sessionModel;
+  let instance;
+
+  beforeEach(done => {
+    axiosGetStub = sinon.stub();
+
+    // we need to proxyquire for multiple dependencies
+    // As soon as you require one of these it tries to create a Redis
+    // connection. We do not want Redis as a dependency of our unit tests.
+    // Therefore stub it before it gets to that
+    const Behaviour = proxyquire('../../../../apps/verify/behaviours/send-verification-email',
+      {
+        '../../ima/index': sinon.stub(),
+        '../../../config': Object.assign({}, config, {
+          login: {
+            appPath: '/ima/start',
+            allowSkip: true,
+            skipEmail: 'test@digital.homeoffice.gov.uk'
+          },
+          saveService: {
+            host: 'https://data-service.com',
+            port: 3001
+          }
+        }),
+        axios: {
+          get: axiosGetStub
+        }
+      });
+    sessionModel = {
+      get: sinon.stub(),
+      set: sinon.stub(),
+      unset: sinon.stub()
+    };
+    res = reqres.res();
+    req = reqres.req({ sessionModel });
+    req.form = { values: {} };
+    res.redirect = sinon.stub();
+
+    const SendVerificationEmail = Behaviour(Controller);
+    instance = new SendVerificationEmail({ template: 'index', route: '/index' });
+    instance._configure(req, res, done);
+  });
+
+  afterEach(async () => {
+    axiosGetStub.reset();
+  });
+
+  describe('getNextStep()', () => {
+    beforeEach(() => {
+      sinon.stub(Controller.prototype, 'getNextStep');
+    });
+    afterEach(() => {
+      Controller.prototype.getNextStep.restore();
+    });
+
+    it('does not call the parent method when email auth is skipped with correct email', () => {
+      req.form.values['user-email'] = 'test@digital.homeoffice.gov.uk';
+
+      instance.getNextStep(req, res);
+
+      Controller.prototype.getNextStep.should.not.have.been.called;
+      res.redirect.should.have.been.calledOnce.calledWithExactly('/ima/start?token=skip');
+    });
+
+    it('calls the parent method when skip email auth is allowed but with incorrect email', () => {
+      req.form.values['user-email'] = 'bad-email@digital.homeoffice.gov.uk';
+
+      instance.getNextStep(req, res);
+
+      Controller.prototype.getNextStep.should.have.been.calledWith(req, res);
+      res.redirect.should.not.have.been.called;
+    });
+  });
+
+  describe('saveValues()', () => {
+    let sandbox;
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+      sandbox.stub(NotifyClient.prototype, 'sendEmail').resolves('email sent');
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+      axiosGetStub.reset();
+    });
+
+    it('sends an email for an application', done => {
+      req.get.withArgs('host').returns('localhost');
+      req.sessionModel.get.withArgs('uan').returns('1876-1234-1234-5678');
+      const data = [];
+      axiosGetStub.resolves({ data: data });
+      req.form.values['user-email'] = 'buzz@lightyear.co.uk';
+
+      instance.saveValues(req, res, () => {
+        NotifyClient.prototype.sendEmail.should.have.been.called;
+        done();
+      }).catch(done);
+    });
+
+    it('throw a validation error if the incorrect email is used for an existing application', done => {
+      req.get.withArgs('host').returns('localhost');
+      req.form.values['user-email'] = 'ronald@test.com';
+      const data = [
+        {
+          id: 12,
+          created_at: '2023-10-09T17:51:38.339Z',
+          updated_at: '2023-10-09T21:59:32.903Z',
+          uan: '9876-1234-1234-5678',
+          email: 'marvel@test.com',
+          date_of_birth: '2000/01/01',
+          session: '{}',
+          expires_at: '2025-10-08'
+        }
+      ];
+      axiosGetStub.resolves({ data: data });
+      instance.saveValues(req, res, err => {
+        err['user-email'].should.be.an.instanceof(instance.ValidationError);
+        err['user-email'].should.have.property('type').and.equal('noRecordMatch');
+        NotifyClient.prototype.sendEmail.should.not.have.been.called;
+        done();
+      })
+        .catch(done);
+    });
+
+    it('skips calling data service when email auth skip is allowed with correct email', done => {
+      req.get.withArgs('host').returns('localhost');
+      req.form = {
+        values: {
+          'user-email': 'test@digital.homeoffice.gov.uk'
+        }
+      };
+      instance.saveValues(req, res, () => {
+        NotifyClient.prototype.sendEmail.should.not.have.been.called;
+        done();
+      }).catch(done);
+    });
+  });
+});
