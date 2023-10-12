2dee39f5e0362053aa2ab0b15450025a1fbc5dfe
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2023-10-12 10:47:56 +0100

    add unit test to pass step on merge

--- server.js
+++ server.js
@@ -19,9 +19,30 @@ const app = hof(settings);
 
 app.use((req, res, next) => {
   res.locals.htmlLang = 'en';
-  res.locals.feedbackUrl = '/feedback';
+  res.locals.feedbackUrl = '/https://eforms.homeoffice.gov.uk/outreach/feedback.ofml';
   next();
 });
+
+if (config.env === 'development' || config.env === 'test') {
+  app.use('/test/bootstrap-session', (req, res) => {
+    const appName = req.body.appName;
+
+    if (!_.get(req, 'session[`hof-wizard-${appName}`]')) {
+      
+      if (!req.session) {
+        throw new Error('Redis is not running!');
+      }
+
+      req.session[`hof-wizard-${appName}`] = {};
+    }
+
+    Object.keys(req.body.sessionProperties || {}).forEach(key => {
+      req.session[`hof-wizard-${appName}`][key] = req.body.sessionProperties[key];
+    });
+    
+    res.send('Session populate complete');
+  });
+}
 
 app.use((req, res, next) => {
   if (req.is('multipart/form-data')) {
