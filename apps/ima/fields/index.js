3e46d4caa8c6367cbbdb79f5a026c26dddb44055
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-10-25 09:30:22 +0100

    Adding pages

--- apps/ima/fields/index.js
+++ apps/ima/fields/index.js
@@ -126,5 +126,68 @@ module.exports = {
       field: 'has-address',
       value: 'no'
     }
+  },
+  'has-legal-representative':{
+    isPageHeading: true,
+  },
+
+  'legal-representative-phone-number': {
+    labelClassName: 'visuallyhidden',
+    validate: ['required', 'internationalPhoneNumber', {type: 'maxlength', arguments: [200]}]
+  },
+  'legal-representative-fullname': {
+    isPageHeading: true,
+    validate: ['required', 'notUrl', {type: 'maxlength', arguments: [200]}]
+  },
+  'legal-representative-organisation':{
+    isPageHeading: true,
+    validate: ['required', 'notUrl', {type: 'maxlength', arguments: [200]}]
+  },
+  'legal-representative-house-number': {
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 200 }],
+    includeInSummary: false
+  },
+  'legal-representative-street': {
+    validate: ['notUrl', { type: 'maxlength', arguments: 200 }],
+    includeInSummary: false
+  },
+  'legal-representative-townOrCity': {
+    validate: ['required', 'notUrl',
+      { type: 'regex', arguments: /^([^0-9]*)$/ },
+      { type: 'maxlength', arguments: 200 }
+    ],
+    includeInSummary: false,
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'legal-representative-county': {
+    validate: ['notUrl',
+      { type: 'regex', arguments: /^([^0-9]*)$/ },
+      { type: 'maxlength', arguments: 200 }
+    ],
+    includeInSummary: false,
+    className: ['govuk-input', 'govuk-!-width-two-thirds']
+  },
+  'legal-representative-postcode': {
+    validate: ['required', 'postcode', {type: 'maxlength', arguments: [200]}],
+    formatter: ['ukPostcode'],
+    includeInSummary: false,
+    className: ['govuk-input', 'govuk-input--width-10'],
+    validationLink: {
+      field: ''
+    }
+  },
+  'someone-else':{
+    isPageHeading: true
+  },
+  'helper-fullname':{
+    mixin: 'input-text',
+    validate: ['required', 'notUrl']
+  },
+  'helper-relationship' :{
+    mixin: 'input-text',
+    validate: ['required', 'notUrl']
+  }, 
+  'helper-organisation':{
+    mixin: 'input-text'
   }
 };
