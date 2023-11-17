b845701c7b9ec40c23bfc8190100177cc1f10aef
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-11-17 14:07:52 +0000

    Added page and url

--- apps/ima/sections/harm-claim-summary.js
+++ apps/ima/sections/harm-claim-summary.js
@@ -3,14 +3,14 @@ const moment = require('moment');
 const PRETTY_DATE_FORMAT = 'DD/MM/YYYY';
 
 module.exports = {
-  'country-claim-against': {
+  'harm-claim-countries': {
     steps: [
       {
-        step: '/country-claim-against',
+        step: '/harm-claim-countries',
         field: ''
       },
       {
-        step: '/serious-and-irreversible-harm',
+        step: '/harm-claim',
         field: 'is-serious-and-irreversible'
       }
     ]
---
+++ apps/ima/views/content/harm-claim-details.md
@@ -0,0 +1,6 @@
+<strong>Explain why you would be at risk of serious and irreversible harm in [SIH country]</strong>
+
+<span class="margin-paragraph">You must tell us:<span>
+- why you specifically would be at risk
+- what you would be at risk of, including what you think would happen to you
+- what individuals or groups you fear in <strong>[SIH country]</strong>
