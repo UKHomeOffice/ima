81e13c52b227f46a5f6a375ae9008c3c2ff4b262
Muhammed Islam <muhammed.islam@digital.homeoffice.gov.uk>
2023-11-22 09:52:13 +0000

    IMB-54: Add screen for removal condition 2
    - add arrival date detail page
    - add HTML files to match the Figma design
    - add validations to all fields
    - add path name for the screen to match the Figma design

--- apps/ima/fields/index.js
+++ apps/ima/fields/index.js
@@ -1,10 +1,49 @@
 'use strict';
 
+const dateComponent = require('hof').components.date;
+const after1900Validator = { type: 'after', arguments: ['1900'] };
+
 module.exports = {
   'who-are-you': {
     isPageHeading: true,
     mixin: 'radio-group',
     options: ['person-named', 'has-legal-representative', 'someone-else'],
     validate: 'required'
+  },
+  'arrival-after-date': {
+    isPageHeading: true,
+    legend: {
+      className: 'visuallyhidden'
+    },
+    mixin: 'radio-group',
+    options: [{
+      value: 'yes'
+    }, {
+      value: 'no',
+      toggle: 'arrival-date-detail-fieldset',
+      child: 'partials/arrival-date-detail'
+    }],
+    validate: 'required'
+  },
+  'arrived-date': dateComponent('arrived-date', {
+    validate: ['required', 'date', after1900Validator, { type: 'before', arguments: '2023-07-20' }],
+    validationLink: {
+      field: 'arrival-after-date',
+      value: 'no'
+    }
+  }),
+  'arrival-details': {
+    labelClassName: 'bold',
+    mixin: 'textarea',
+    validate: ['required', 'notUrl', { type: 'regex', arguments: /^[^\[\]\|<>]*$/ },
+      { type: 'maxlength', arguments: 15000 }],
+    dependent: {
+      field: 'arrival-after-date',
+      value: 'no'
+    },
+    attributes: [{
+      attribute: 'rows',
+      value: 6
+    }]
   }
 };
