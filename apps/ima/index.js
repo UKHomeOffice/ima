de7d380b4c32c07318f7c0c59b563ea548a4b93e
Muhammed Islam <muhammed.islam@digital.homeoffice.gov.uk>
2023-12-05 19:08:53 +0000

    IMB-57: Add screens for Human rights section
    - add screens for the 'human rights' section
    - add path names for the new screens
    - add new fields for the screens
    - add partial for the 'Other' UK immigration status detail input, to match the Figma design
    - add validation to all fields
    - make validation dynamic for checkboxes and immigration status field dependent on user selections
    - add logic for human rights family screen table
    - include sections in the main summary page
    - add conditional logic for determining flow between screens
    - amend design and logic to match latest content design changes
    - add unit tests to cover the looping scenarios

--- apps/ima/index.js
+++ apps/ima/index.js
@@ -8,6 +8,10 @@ const CheckEmailToken = require('./behaviours/check-email-token');
 const ResumeSession = require('./behaviours/resume-form-session');
 const SaveFormSession = require('./behaviours/save-form-session');
 const SaveAndExit = require('./behaviours/save-and-exit');
+const AggregateSaveUpdate = require('./behaviours/aggregator-save-update');
+const FamilyMembersLocals = require('./behaviours/family-members-locals');
+const ModifyChangeURL = require('./behaviours/modify-change-link');
+const ResetHumanRightsSummary = require('./behaviours/reset-human-rights-summary');
 
 module.exports = {
   name: 'ima',
@@ -33,11 +37,73 @@ module.exports = {
       behaviours: SaveFormSession,
       fields: ['who-are-you'],
       locals: { showSaveAndExit: true },
-      next: '/confirm', // TO BE UPDATED AS STEPS ARE ADDED
+      next: '/human-rights-claim', // TO BE UPDATED AS STEPS ARE ADDED
       backLink: 'current-progress'
     },
+    '/human-rights-claim': {
+      behaviours: [ResetHumanRightsSummary, SaveFormSession],
+      fields: ['human-rights-claim'],
+      locals: { showSaveAndExit: true },
+      forks: [
+        {
+          target: '/human-rights-family',
+          condition: {
+            field: 'human-rights-claim',
+            value: 'yes'
+          }
+        }
+      ],
+      continueOnEdit: true,
+      next: '/other-human-rights-claims'
+    },
+    '/human-rights-family': {
+      behaviours: SaveFormSession,
+      fields: ['family-member-relation',
+        'family-member-full-name',
+        'family-member-dob',
+        'family-member-nationality',
+        'uk-immigration-status',
+        'immigration-status-other',
+        'reference-number-option',
+        'uan-detail',
+        'ho-number-detail',
+        'human-rights-claim-details'],
+      locals: { showSaveAndExit: true },
+      continueOnEdit: true,
+      next: '/human-rights-family-summary'
+    },
+    '/human-rights-family-summary': {
+      behaviours: [AggregateSaveUpdate, FamilyMembersLocals, SaveFormSession],
+      aggregateTo: 'family-members',
+      aggregateFrom: [
+        'family-member-full-name',
+        'family-member-relation',
+        'family-member-dob',
+        'family-member-nationality',
+        'uk-immigration-status',
+        'immigration-status-other',
+        'reference-number-option',
+        'uan-detail',
+        'ho-number-detail',
+        'human-rights-claim-details'
+      ],
+      titleField: 'family-member-full-name',
+      addStep: 'human-rights-family',
+      addAnotherLinkText: 'family member',
+      template: 'family-members-summary',
+      locals: { showSaveAndExit: true },
+      continueOnEdit: true,
+      next: '/other-human-rights-claims'
+    },
+    '/other-human-rights-claims': {
+      behaviours: [SaveFormSession],
+      fields: ['other-human-rights-claim', 'other-human-rights-claim-details'],
+      locals: { showSaveAndExit: true },
+      continueOnEdit: false,
+      next: '/confirm' // TO BE UPDATED AS STEPS ARE ADDED
+    },
     '/confirm': {
-      behaviours: [Summary, SaveFormSession],
+      behaviours: [Summary, ModifyChangeURL, SaveFormSession],
       sections: require('./sections/summary-data-sections'),
       locals: { showSaveAndExit: true },
       next: '/confirmation'
