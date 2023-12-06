d830d833be485567cc592c8ecbf7cb87918ca28f
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-12-06 23:18:54 +0000

    IMB-32 (Personal details pages) and IMB-18 (Save and Exit pages)
    IMB-18 (Save and Exit)
    - amend save and exit template
    - added functionality to send email on save and exit in save-form-session.js
    - created template Id use for Gov Notify
    
    IMB-32 (Personal details)
    - added validations for each input field
    - added url path and declare fields name in Ima/index.js.
    - created field for every input component in field.js.
    - added labels and text for each pages also created mark down files .
    - added css.

--- apps/ima/sections/summary-data-sections.js
+++ apps/ima/sections/summary-data-sections.js
@@ -17,5 +17,64 @@ module.exports = {
         omitChangeLink: true
       }
     ]
+  },
+  'immigration-adviser-details': {
+    steps: [
+      {
+        step: '/who-are-you',
+        field: 'who-are-you'
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-fullname',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return null;
+        }
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-email',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return req.sessionModel.get('is-legal-representative-email') === 'yes' ? `${req.sessionModel.get('user-email')}` : `${req.sessionModel.get('legal-representative-email')}`;
+        }
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-phone-number'
+      },
+      {
+        step: '/immigration-adviser-details',
+        field: 'legal-representative-address',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/immigration-adviser-details')) {
+            return null;
+          }
+          return `${req.sessionModel.get('legal-representative-house-number')} \n ${req.sessionModel.get('legal-representative-street')} \n ${req.sessionModel.get('legal-representative-townOrCity')}\n${req.sessionModel.get('legal-representative-county')}\n${req.sessionModel.get('legal-representative-postcode')}`;
+        }
+      }
+    ]},
+  'personal-details': {
+    steps: [
+      {
+        step: '/full-name',
+        field: 'name'
+      },
+      {
+        step: '/email',
+        field: 'email-address',
+        parse: (list, req) => {
+          if (!req.sessionModel.get('steps').includes('/email')) {
+            return null;
+          }
+          return req.sessionModel.get('email-address') === 'no' ? `${req.sessionModel.get('user-email')}` : `${req.sessionModel.get('email-address-details')}`;
+        }
+      }
+    ]
   }
 };
