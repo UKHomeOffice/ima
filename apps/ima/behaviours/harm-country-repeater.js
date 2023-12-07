4a8a2becd450d19bda9029e9a9bd889b1307cf34
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-12-07 12:46:34 +0000

    Adding event

--- apps/ima/behaviours/harm-country-repeater.js
+++ apps/ima/behaviours/harm-country-repeater.js
@@ -2,7 +2,7 @@
 
 module.exports = superclass => class extends superclass {
   saveValues(req, res, next) {
-      
+    console.log(next)
     req.form.values['harm-claim-countries'] = [
       req.form.values['country-1'],
       req.form.values['country-2'],
@@ -13,14 +13,10 @@ module.exports = superclass => class extends superclass {
     
    req.sessionModel.set('country-count', req.sessionModel.get('harm-claim-countries'));
    
-    
-    
-   console.log(req.form.values['harm-claim-countries'])
-    return super.saveValues(req, res, next);
+   return super.saveValues(req, res, next);
   }
 
   getValues(req, res, next) {
-   
     super.getValues(req, res, (err, values) => {
       const harmClaimCountries = req.sessionModel.get('harm-claim-countries') || [];
       values['country-1'] = harmClaimCountries[0] || '';
@@ -29,7 +25,7 @@ module.exports = superclass => class extends superclass {
       values['country-4'] = harmClaimCountries[3] || '';
       values['country-5'] = harmClaimCountries[4] || '';
 
-    //  console.log(harmClaimCountries)
+    console.log(harmClaimCountries)
       next(err, values);
     });
   }
--- assets/js/event-listeners.js
+++ assets/js/event-listeners.js
@@ -3,9 +3,18 @@
 
 const init = function init() {
    
-    // document.getElementById("country-1").addEventListener("change", function(e){
-    //     return e.target.value
-    //   })
+   
+    document.getElementById("country-1").addEventListener("change", function(e,req){
+  
+     // fetch('http://localhost:8080/ima/harm-claim-countries');
+      //alert("Do you want to change country")
+     $.ajax({
+      type: "GET",
+      url: 'http://localhost:8080/ima/harm-claim-countries'
+    });
+    //    req.sessionModel.set('harm-claim-country',e.target.value);
+     //   alert("Do you want to change country")
+    })
  
 };
 
