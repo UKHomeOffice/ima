83c8c07b071a1f87ecc2990c3b99cdbeb86213cf
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-12-08 08:51:44 +0000

    Update each country

--- apps/ima/behaviours/harm-countries-name.js
+++ apps/ima/behaviours/harm-countries-name.js
@@ -1,14 +1,22 @@
 const _ = require("lodash");
 module.exports = superclass => class extends superclass {
+  saveValues(req, res, next) {
+    
+   
+   
+   return super.saveValues(req, res, next);
+  }
+
+
 
     getValues(req,res,next){
      
-      if(req.sessionModel.get('country-count') === undefined){
-        var countryCount = [...req.sessionModel.get('harm-claim-countries')]
-        req.sessionModel.set('country-count', countryCount);
+      if(req.sessionModel.get('harm-country-state') === undefined){
+        var countryState = [...req.sessionModel.get('harm-claim-countries')]
+        req.sessionModel.set('harm-country-state', countryState);
       }
      
-      var countryName = req.sessionModel.get('country-count')[0];
+      var countryName = req.sessionModel.get('harm-country-state')[0];
       req.sessionModel.set('country-placeholder', countryName);
    
       return super.getValues(req,res,next)
