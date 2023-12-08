7d2ccacc4605eb74300abf7e62afc21ff8b1dd67
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2023-12-08 15:13:11 +0000

    Added country text for updating

--- apps/ima/behaviours/country-name.js
+++ apps/ima/behaviours/country-name.js
@@ -4,33 +4,49 @@ const _ = require("lodash");
 
 module.exports = superclass => class extends superclass {
 
-    locals(req, res) {
-        const locals = super.locals(req, res);
-        
-         
-    // render country number on country-details page
-        _.forEach(locals.items, (i) => {
-         
-          const countryNumber = _.indexOf(locals.items, i);
-       
-        req.sessionModel.get('sih-countries').aggregatedValues
-          var countryName =  req.sessionModel.get('harm-claim-countries');
-          req.form.values['country-name'] = countryName[countryNumber];
+  locals(req, res) {
+    const locals = super.locals(req, res);
 
-          i.itemTitle = countryName[countryNumber]
-          _.forEach(i.fields, (field) => {
-            if (field.field === 'countryAddNumber') {
-              field.value = countryNumber
-              field.parsed = countryNumber
-            }
-            if (field.field === 'why-not-get-protection') {
-            locals['country-name']= countryName[countryNumber];
-              field.field += '.summary-heading';
-            }
-          })
-        })
-        return locals;
-      }
-    };
-    
+
+    // render country number on country-details page
+    _.forEach(locals.items, (i) => {
+
+      const countryNumber = _.indexOf(locals.items, i);
+
+      req.sessionModel.get('sih-countries').aggregatedValues
+      var countryName = req.sessionModel.get('harm-claim-countries');
+      req.form.values['country-name'] = countryName[countryNumber];
+
+      i.itemTitle = countryName[countryNumber]
+      _.forEach(i.fields, (field) => {
+        if (field.field === 'countryAddNumber') {
+          field.value = countryNumber
+          field.parsed = countryNumber
+        }
+        if ((field.field === 'why-not-get-protection') && (countryNumber === 0)) {
+          locals['country-name'] = countryName[countryNumber];
+          field.field += '.summary-heading1';
+        }
+        if ((field.field === 'why-not-get-protection') && (countryNumber === 1)) {
+          locals['country-name'] = countryName[countryNumber];
+          field.field += '.summary-heading2';
+        }
+        if ((field.field === 'why-not-get-protection') && (countryNumber === 2)) {
+          locals['country-name'] = countryName[countryNumber];
+          field.field += '.summary-heading3';
+        }
+        if ((field.field === 'why-not-get-protection') && (countryNumber === 3)) {
+          locals['country-name'] = countryName[countryNumber];
+          field.field += '.summary-heading4';
+        }
+        if ((field.field === 'why-not-get-protection') && (countryNumber === 4)) {
+          locals['country-name'] = countryName[countryNumber];
+          field.field += '.summary-heading5';
+        }
+      })
+    })
+    return locals;
+  }
+};
+
 
