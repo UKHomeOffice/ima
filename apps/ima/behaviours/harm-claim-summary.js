dcf6dd39599382d0d04c51d6ed9f7eb8aefdc90f
muthrajan <mylrajan.muthusamy1@digital.homeoffice.gov.uk>
2024-04-12 12:45:28 +0100

    IMB-130 harm-claim-summary changes

--- apps/ima/behaviours/harm-claim-summary.js
+++ apps/ima/behaviours/harm-claim-summary.js
@@ -1,44 +1,13 @@
-// const _ = require('lodash');
-
-// // Additional person fields are optional, so this behaviour sets each additional
-// // person a number so as to be identifiable.
-// module.exports = superclass => class extends superclass { 
-//   locals(req, res) {
-//     const locals = super.locals(req, res);
-//     // render country number on country-details page
-//     _.forEach(locals.items, i => {
-//       const countryNumber = _.indexOf(locals.items, i);
-
-//       _.forEach(i.fields, field => {
-//         if (field.field === 'countryAddNumber') {
-//           field.value = countryNumber;
-//           field.parsed = countryNumber;
-//         }
-
-//         if (field.field === 'why-not-get-protection') {
-//           field.field += '.summary-heading1';
-//         }
-//       });
-//     });
-//     console.log("locals = " + JSON.stringify(locals))
-//     return locals;
-//   }
-// };
 const _ = require('lodash');
 
 module.exports = superclass => class extends superclass {
-  // configure(req, res, next){
-  //   if(req.form.options.route === '/harm-claim-summary') {
-  //     console.log("Hello1 = " + JSON.stringify(req.query));
-  //     console.log("Hello = " + JSON.stringify(req.sessionModel.get('sih-countries'))); 
-  //     console.log("Hello = " + req.sessionModel.get('sih-countries').aggregatedValues.length)
-  //     // if(req.sessionModel.get('sih-countries').aggregatedValues.length === 0) {
-  //     //   console.log("Hello = " + req.sessionModel.get('sih-countries').aggregatedValues.length)
-  //     //   return res.redirect(`${req.baseUrl}/harm-claim`); 
-  //     // }
-  //   }
-  //   next()
-  // }
+  configure(req, res, next) {
+    if(req.sessionModel.get('sih-countries') && !req.sessionModel.get('sih-countries').aggregatedValues.length) {
+      req.form.options.addStep ='harm-claim';
+     }
+     super.configure(req, res, next)
+  }
+
   locals(req, res) {
     const locals = super.locals(req, res);
     _.forEach(locals.items, i => {
@@ -80,11 +49,6 @@ module.exports = superclass => class extends superclass {
         }
       });
     });
-    if(req.sessionModel.get('sih-countries').aggregatedValues.length === 0) {
-     locals.addStep='harm-claim';
-    }
-
-    console.log("locals = " +JSON.stringify(locals))
     return locals;
   }
 };
