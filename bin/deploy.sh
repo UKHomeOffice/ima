6c31e142f8e8407d22d20955961d8b494dd2f37a
adityababumallisettiHO <146218064+adityababumallisettiHO@users.noreply.github.com>
2024-05-20 17:28:42 +0100

    IMB-111: Update Ingress for data-service (#122)
    * IMB-111: Update Ingress for data-service
    
    * Replace ingress-external with ingress-internal
    * Internal-ingress is for backend services which are not exposed to outside the network
    * Internal Ingress annotations has been added to Data service
    * Update deploy.sh to use internal ingress annotations

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ export INGRESS_INTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-internal-annotations.yam
 export INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/ingress-external-annotations.yaml
 export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
-export DATA_SERVICE_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/data-service-external-annotations.yaml
+export DATA_SERVICE_INTERNAL_ANNOTATIONS=$HOF_CONFIG/data-service-internal-annotations.yaml
 export FILEVAULT_NGINX_SETTINGS=$HOF_CONFIG/filevault-nginx-settings.yaml
 export FILEVAULT_INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/filevault-ingress-external-annotations.yaml
 
